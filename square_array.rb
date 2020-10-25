7f047c1c0b89060db939cf3c081e266e31375558
bambi teague <teeegsa@gmail.com>
2020-10-25 15:13:54 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,28 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+   arr = []
+   numbers.each {|i| arr << i ** 2}
+   arr
 end
+
+
+
+
+
+# does not call on collect/map/inject
+# calls on each
+# square the elements in the array
+
+
+#EXPECTATIONS OF TEST:
+
+#numbers = [1,2,3]
+
+#square_array(numbers)
+# => [1,4,9])
+
+#new_numbers = [9,10,16,25]
+
+#square_array(new_numbers)
+# => [81,100,256,625] 
+
+
